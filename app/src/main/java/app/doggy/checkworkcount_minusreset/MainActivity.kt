1794e3ac89573e9e6db7b3e4b1a7187360108fdb
Kaito-Dogi <kendobu0405@gmail.com>
2022-01-05 19:51:12 +0900

    add: View Bindingの設定

--- app/src/main/java/app/doggy/checkworkcount_minusreset/MainActivity.kt
+++ app/src/main/java/app/doggy/checkworkcount_minusreset/MainActivity.kt
@@ -2,10 +2,15 @@ package app.doggy.checkworkcount_minusreset
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.doggy.checkworkcount_minusreset.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // バインディングクラスの変数
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
     }
 }
